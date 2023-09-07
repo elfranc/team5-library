305649ff5e0c0a2014de42ceb821ce66ca1bb5e1
Francis <elfrancteetwo@gmail.com>
2023-09-07 12:08:58 -0400

    new groovy file added

--- vars/teamApp.groovy
+++ vars/teamApp.groovy
@@ -1,1 +1,31 @@
-#
+def call(string repoUrl)
+pipeline {
+       agent any
+       tools {
+           maven 'maven'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'mvn --version'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage('team5-test-shared'){
+            steps{
+                echo "lets test this"
+            }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'mvn -v'
+               }
+           }
+       }
+}
